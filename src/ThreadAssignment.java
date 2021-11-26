60bd0ea20d2a516d2bf22f1819918655883e8b00
MedAziz11 <chagourmedaziz@gmail.com>
2021-11-26 17:07:01 +0100

    implementing counter

--- src/ThreadAssignment.java
+++ src/ThreadAssignment.java
@@ -3,7 +3,12 @@ public class ThreadAssignment {
     static class Counter {
         void count() {
             //todo implement me
+            for (int i = 350; i>=1; i--){
+                System.out.println(i);
+            }
+            System.out.println("FINISH !!");
         }
+
     }
 
     static class MyThread extends Thread {
@@ -15,14 +20,27 @@ public class ThreadAssignment {
 
         @Override
         public void run() {
-            counter.count();
+            synchronized (counter){
+                counter.count();
+            };
         }
     }
 
     public static void main(String[] args) {
         Counter counter = new Counter();
 
-        new MyThread(counter).start();
-        new MyThread(counter).start();
+        MyThread t1 = new MyThread(counter);
+        MyThread t2 = new MyThread(counter);
+        t1.start();
+        t2.start();
+
+        try {
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("DONE!!");
+
     }
 }
